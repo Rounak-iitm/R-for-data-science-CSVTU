9e6e18bdac18af613b54f8ccb2fd40dfbf36c37c
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:54:43 +0530

    Create Q12.r

---
+++ assignment_2/Q12.r
@@ -0,0 +1,4 @@
+# 12. Use a fourfold repetition of the second row of the matrix formed in (11.)
+array_12 <- array(rep(result_11[2, ], each = 4), dim = c(2, 2, 2, 3))
+print(array_12)
+# Output: A 2 x 2 x 2 x 3 array
