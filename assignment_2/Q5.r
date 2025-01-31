3879b8b17494622f6b48df621984dc0b1000aebb
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:50:47 +0530

    Create Q5.r

---
+++ assignment_2/Q5.r
@@ -0,0 +1,7 @@
+# 5. Store the bottom four elements as a new 2 x 2 matrix
+matrix_5 <- matrix(matrix_1[3:4, ], nrow = 2, ncol = 2)
+print(matrix_5)
+# Output:
+#      [,1] [,2]
+# [1,] 1.6  8.2
+# [2,] 3.2  6.5
