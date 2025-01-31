3a82b0954f63717c48c60a5b68d64818182949d2
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:58:56 +0530

    Create Q21.r

---
+++ assignment_2/Q21.r
@@ -0,0 +1,7 @@
+# 21. Calculate the operation
+result_21 <- 3 * (matrix(c(2, 5, 3, 7), nrow = 2) - matrix(c(5, 10, 15, 20), nrow = 2))
+print(result_21)
+# Output:
+#      [,1] [,2]
+# [1,] -9  -15
+# [2,] -6  -9
