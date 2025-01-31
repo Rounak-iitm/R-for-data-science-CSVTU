4584aa52089fbf234cd157a02cbfb788cf926b03
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:57:31 +0530

    Create Q18.r

---
+++ assignment_2/Q18.r
@@ -0,0 +1,10 @@
+# 18. What would the result be if you remove the second row and the third column from the sorted matrix
+result_18 <- matrix_17[-2, -3, drop = FALSE]
+print(result_18)
+# Output:
+#      [,1] [,2]
+# [1,]    5   10
+# [2,]    4    9
+# [3,]    3    8
+# [4,]    1    6
+# [5,]   16   17
