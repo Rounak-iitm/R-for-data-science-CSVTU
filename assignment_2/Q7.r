71365aca73c31de2264b7b4813817aa9299755fe
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:51:56 +0530

    Create Q7.r

---
+++ assignment_2/Q7.r
@@ -0,0 +1,4 @@
+# 7. Calculate the operation
+result_7 <- (2/7) * (matrix(c(1, 2, 2, 4, 7, 6), nrow = 3) - matrix(c(10, 20, 30, 40, 50, 60), nrow = 3))
+print(result_7)
+# Output: A 3 x 2 matrix with the calculated values
