e3143f30368e870bd96bc0abb233642a9cad58f8
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:56:33 +0530

    Create Q16.r

---
+++ assignment_2/Q16.r
@@ -0,0 +1,4 @@
+# 16. If you add a new row to the matrix created in question (15.), confirm its new dimensions
+matrix_15 <- rbind(matrix_15, c(16, 17, 18))  # Adding a new row
+dim(matrix_15)
+# Output: [1] 6 3
