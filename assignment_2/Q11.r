e3a3f883d96aa765a1743db8cfd1a25a386d9036
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-01-31 18:54:17 +0530

    Create Q11.r

---
+++ assignment_2/Q11.r
@@ -0,0 +1,4 @@
+# 11. Extract and store the fourth- and first-row elements of the second column
+result_11 <- array_10[c(4, 1), 2, ]
+print(result_11)
+# Output: A 2 x 6 matrix with the specified elements
