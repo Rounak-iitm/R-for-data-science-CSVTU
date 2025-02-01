3cb71aabadb17f42f1bcde2950a4d59084551342
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-02-02 00:56:33 +0530

    Update Q1.r

--- assignment_2/Q1.r
+++ assignment_2/Q1.r
@@ -1,6 +1,8 @@
 # 1. Construct and store a 4 x 2 matrix filled row-wise
+
 matrix_1 <- matrix(c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, 6.5), nrow = 4, ncol = 2, byrow = TRUE)
 print(matrix_1)
+
 # Output:
 #      [,1] [,2]
 # [1,] 4.3  3.1
