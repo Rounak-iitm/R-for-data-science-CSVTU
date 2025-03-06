bc7587c7b3e509621cee27e4a34f124d91344e07
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-03-06 09:13:10 +0530

    Create q12.r
    Signed-off-by: Rounak Gupta  <167229362+Rounak-iitm@users.noreply.github.com>

---
+++ assignment 3/q12.r
@@ -0,0 +1,8 @@
+# Convert character vector to a factor and display levels:
+char_vector <- c("male", "female", "male", "female")
+factor_vector <- factor(char_vector)
+print(factor_vector)
+print(levels(factor_vector))
+# output 
+[1] male   female male   female
+Levels
