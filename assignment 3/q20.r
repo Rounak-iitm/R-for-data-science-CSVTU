01aa23895ba3f5c4fb833df6c50557fed67b6e57
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-03-06 09:36:52 +0530

    Update q20.r
    Signed-off-by: Rounak Gupta  <167229362+Rounak-iitm@users.noreply.github.com>

--- assignment 3/q20.r
+++ assignment 3/q20.r
@@ -1,3 +1,6 @@
 # Create and Print a String with Escape Sequences
 escaped_string <- "This is a newline:\nThis is a tab:\tAnd this is a backslash:\\"
 cat(escaped_string)
+#output
+This is a newline:
+This is a tab:   And this is a backslash:\
