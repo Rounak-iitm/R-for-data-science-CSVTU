e6e5b6f72f8f09b39c9916cb91b45d4c7ba06941
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-03-06 09:30:12 +0530

    Create q16.r
    Signed-off-by: Rounak Gupta  <167229362+Rounak-iitm@users.noreply.github.com>

---
+++ assignment 3/q16.r
@@ -0,0 +1,9 @@
+ # Demonstrate any() and all() Functions
+logical_vector <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
+any_true <- any(logical_vector)
+print(any_true)
+all_true <- all(logical_vector) 
+print(all_true)
+# output 
+[1] TRUE
+[1] FALSE
