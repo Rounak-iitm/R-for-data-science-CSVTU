1bf6d372642e8fdf6507c608bd67b39ce9f7d26b
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-03-06 08:45:36 +0530

    Create q1.r
    Signed-off-by: Rounak Gupta  <167229362+Rounak-iitm@users.noreply.github.com>

---
+++ assignment 3/q1.r
@@ -0,0 +1,5 @@
+# logical vector alteernating between true and false: 
+logicaL_vec <- c(TRUE, FALSE ,TRUE , FALSE , TRUE ,FALSE , TRUE,FALSE,TRUE,FALSE,TRUE,FALSE)
+print(logical_vec)
+# output
+[1] TRUE FALSE TRUE FALSE TRUE FALSE TRUE FALSE TRUE FALSE TRUE FALSE 
