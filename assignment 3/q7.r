4d3a0b18d643df50430c83471465f6d121e7c99a
Rounak Gupta <167229362+Rounak-iitm@users.noreply.github.com>
2025-03-06 09:07:55 +0530

    Create q7.r
    Signed-off-by: Rounak Gupta  <167229362+Rounak-iitm@users.noreply.github.com>

---
+++ assignment 3/q7.r
@@ -0,0 +1,6 @@
+# Extract elements greater than 10 using logical subsetting:
+num_vector <- c(5, 11, 8, 15, 22, 7)
+greater_than_10 <- num_vector[num_vector > 10]
+print(greater_than_10)
+# output
+[1] 11 15 22
